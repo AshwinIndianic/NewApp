d410a0219685a6ccf38ed89423f87e8cf57bd7b0
indianic <indianic@ind508.local>
2016-09-10 17:35:18 +0530

    test commit1

---
+++ NewApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewApp
+//
+//  Created by indianic on 10/09/16.
+//  Copyright © 2016 indianic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
